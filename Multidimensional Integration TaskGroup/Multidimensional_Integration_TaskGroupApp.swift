049fa1066af9a9891b6263643e27dd12a26896df
jterry94 <terryj@iit.edu>
2024-01-16 18:26:50 -0600

    Initial Commit

---
+++ Multidimensional Integration TaskGroup/Multidimensional_Integration_TaskGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multidimensional_Integration_TaskGroupApp.swift
+//  Multidimensional Integration TaskGroup
+//
+//  Created by Jeff_Terry on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Multidimensional_Integration_TaskGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
